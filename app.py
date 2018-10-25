852e96589f4f1a1cd0f270b9d8f0a0bb32c5e7b5
Jakub Zurakowski <Jakub_zzzz@hotmail.com>
2018-10-25 22:28:18 +0100

    Page lists all files and students. No idea how to return the files tho.

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import abort
+from functools import reduce
+
+app = Flask(__name__)
+
+
+def get_directory_structure(rootdir):
+    """
+    Creates a nested dictionary that represents the folder structure of rootdir
+    """
+    dir = {}
+    rootdir = rootdir.rstrip(os.sep)
+    start = rootdir.rfind(os.sep) + 1
+    for path, dirs, files in os.walk(rootdir):
+        folders = path[start:].split(os.sep)
+        subdir = dict.fromkeys(files)
+        parent = reduce(dict.get, folders[:-1], dir)
+        parent[folders[-1]] = subdir
+    return dir
+
+
+@app.route('/')
+def index():
+    files = get_directory_structure("student_work")
+    return render_template("index.html", files=files)
+
+
+@app.route('/<term>/<student>/<file>')
+def get_student_file(term, student, file):
+    if not os.path.isfile(f"student_work/{term}/{student}/{file}"):
+        abort(404)
+    return render_template(f"../student_work/{term}/{student}/{file}")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
